4167e90b05679e4cacac417aaba65a4286d81f36
melomanos <philippov_mihail@mail.ru>
2020-11-18 21:14:54 +0300

    JUnit4 Platform done

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -27,4 +27,26 @@ public class CashbackHackServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @org.junit.jupiter.api.Test
+    public void shouldTestRemainNotMultiple1000JUnitJupiter() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 7700;
+
+        int actual = service.remain(amount);
+        int expected = 300;
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void shouldTestRemainMultiple1000JUnitJupiter() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 4000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
 }
