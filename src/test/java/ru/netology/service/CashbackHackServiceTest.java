fadf8a10917779f9949d5f7c14a2b6327dee5fb3
melomanos <philippov_mihail@mail.ru>
2020-11-18 20:58:22 +0300

    JUnit4 done

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,30 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldTestRemainNotMultiple1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 7700;
+
+        int actual = service.remain(amount);
+        int expected = 300;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldTestRemainMultiple1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 4000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+}
