70e81bc4408af9ec623e1fbd5b4d34146c3ad601
melomanos <philippov_mihail@mail.ru>
2020-11-22 14:33:17 +0300

    New test added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -27,4 +27,15 @@ public class CashbackHackServiceTest {
 
         assertEquals(actual, expected);
     }
+
+    @Test
+    public void shouldTestRemainMultiple1000Less1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 400;
+
+        int actual = service.remain(amount);
+        int expected = 600;
+
+        assertEquals(actual, expected);
+    }
 }
